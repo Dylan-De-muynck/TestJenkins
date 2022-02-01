01950caca9d83f5f75e48437d308942b97a5c16d
Dylan De muynck <dylan.demuynck1@epsi.fr>
2022-02-01 14:53:11 +0100

    azezeaze

--- test/CalculsTest2.java
+++ test/CalculsTest2.java
@@ -34,6 +34,24 @@ class CalculsTest2 {
         		Arguments.of(3,2,1)
         );
     }
+    
+    static Stream<Arguments> chargerAddition() throws Throwable 
+    {
+        return Stream.of(
+        		Arguments.of(2,2,4), 
+        		Arguments.of(6,6,12), 
+        		Arguments.of(3,2,5)
+        );
+    }
+    
+    static Stream<Arguments> chargerSoustraction() throws Throwable 
+    {
+        return Stream.of(
+        		Arguments.of(2,2,0), 
+        		Arguments.of(6,6,0), 
+        		Arguments.of(3,2,1)
+        );
+    }
 
 	@ParameterizedTest(name="Multiplication numéro {index}: nombre1={0}, nombre2={1}, r�sultat attendu = {2}")
 	@MethodSource("chargerMultiplication")
@@ -54,7 +72,33 @@ class CalculsTest2 {
 	{
 		// Partie paramétrée
 	        Calculs monCal = new Calculs(firstNumber, secondNumber);
-	        assertEquals(expectedResult, monCal.diviser(), "test en echec pour " + firstNumber + " * " + secondNumber + " != " + expectedResult); 
+	        assertEquals(expectedResult, monCal.diviser(), "test en echec pour " + firstNumber + " / " + secondNumber + " != " + expectedResult); 
+
+	    // Partie indépendante (les paramétres peuvent ne servir qu'à une sous partie des tests)
+	        String n = null;
+	        assertNull(n);
+	}
+	
+	@ParameterizedTest(name="Addition numéro {index}: nombre1={0}, nombre2={1}, r�sultat attendu = {2}")
+	@MethodSource("chargerAddition")
+	void testAddition(int firstNumber, int secondNumber, int expectedResult) 
+	{
+		// Partie paramétrée
+	        Calculs monCal = new Calculs(firstNumber, secondNumber);
+	        assertEquals(expectedResult, monCal.additionner(), "test en echec pour " + firstNumber + " + " + secondNumber + " != " + expectedResult); 
+
+	    // Partie indépendante (les paramétres peuvent ne servir qu'à une sous partie des tests)
+	        String n = null;
+	        assertNull(n);
+	}
+	
+	@ParameterizedTest(name="Soustraction numéro {index}: nombre1={0}, nombre2={1}, r�sultat attendu = {2}")
+	@MethodSource("chargerSoustraction")
+	void testSoustraction(int firstNumber, int secondNumber, int expectedResult) 
+	{
+		// Partie paramétrée
+	        Calculs monCal = new Calculs(firstNumber, secondNumber);
+	        assertEquals(expectedResult, monCal.soustraire(), "test en echec pour " + firstNumber + " - " + secondNumber + " != " + expectedResult); 
 
 	    // Partie indépendante (les paramétres peuvent ne servir qu'à une sous partie des tests)
 	        String n = null;
